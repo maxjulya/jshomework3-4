c986211c778d862e488db26fcc49bc9a6cf19168
maxjulya <maxjulya@gmail.com>
2017-01-19 14:13:06 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// var div = document.createElement('div');
+// div.className = "alert alert-success";
+// div.innerHTML = "<strong>Ура!</strong> Вы прочитали это важное сообщение.";
+//
+// document.body.appendChild(div);
+//
+// setTimeout(function() {
+//   div.parentNode.removeChild(div);
+// }, 1000);
+
